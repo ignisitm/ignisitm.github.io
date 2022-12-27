3b9f0e05372467a57e4bef74fae5e52520288d3a
saathvika <saathvika.nt@gmail.com>
2022-12-27 16:57:54 +0300

    Added generate invoice modal feilds

--- src/Pages/Invoice/GenerateInvoice.tsx
+++ src/Pages/Invoice/GenerateInvoice.tsx
@@ -1,4 +1,14 @@
-import { Button, Form, Input, message, Modal, Select } from "antd";
+import {
+  Button,
+  Form,
+  Input,
+  message,
+  Modal,
+  Select,
+  Row,
+  Col,
+  Table,
+} from "antd";
 import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
 import { ExclamationCircleOutlined } from "@ant-design/icons";
 import { FC, useState, useContext } from "react";
@@ -28,12 +38,54 @@ const GenerateInvoiceForm: FC<GenerateInvoiceFormProps> = ({
   const [form] = Form.useForm();
   const [modal, contextHolder] = Modal.useModal();
 
+  // interface DataType {
+  //   key: string;
+  //   name: string;
+  //   age: number;
+  //   address: string;
+  //   tags: string[];
+  // }
+
+  const columns: any = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      // render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+  ];
+
+  const data: any[] = [
+    {
+      key: "1",
+      name: "John Brown",
+      age: 32,
+      address: "New York No. 1 Lake Park",
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      age: 42,
+      address: "London No. 1 Lake Park",
+    },
+  ];
+
   return (
     <Modal
       visible={visible}
       style={{ top: "15px" }}
-      title="Create Notification"
-      okText="Create Notification"
+      title="Invoicing Details"
+      okText="Generate"
       cancelText="Cancel"
       onCancel={() => {
         form.resetFields();
@@ -54,7 +106,200 @@ const GenerateInvoiceForm: FC<GenerateInvoiceFormProps> = ({
       confirmLoading={confirmLoading}
     >
       {contextHolder}
-      <Form form={form} layout="vertical" name="form_in_modal"></Form>
+      <Form form={form} layout="vertical" name="form_in_modal">
+        <Row>
+          <Col md={12} xs={24} style={{ paddingRight: "10px" }}>
+            <Form.Item
+              label="Invoice No."
+              name="invoice_no"
+              rules={[
+                {
+                  // required: props.requiredFields.contract_number,
+                  message: "Please enter the Invoice No.",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col md={12} xs={24} style={{ paddingRight: "10px" }}>
+            <Form.Item
+              label="Job Code"
+              name="job_code"
+              rules={[
+                {
+                  // required: props.requiredFields.contract_number,
+                  message: "Please enter the Job Code",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} xs={24} style={{ paddingRight: "10px" }}>
+            <Form.Item
+              label="Invoice Date"
+              name="invoice_date"
+              rules={[
+                {
+                  // required: props.requiredFields.contract_number,
+                  message: "Please enter the invoice date",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col md={12} xs={24} style={{ paddingRight: "10px" }}>
+            <Form.Item
+              label="Job description"
+              name="job_description"
+              rules={[
+                {
+                  // required: props.requiredFields.contract_number,
+                  message: "Please enter the job description",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} xs={24} style={{ paddingRight: "10px" }}>
+            <Form.Item
+              label="Customer"
+              name="customer"
+              rules={[
+                {
+                  // required: props.requiredFields.contract_number,
+                  message: "Please enter the customer name",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col md={12} xs={24} style={{ paddingRight: "10px" }}>
+            <Form.Item
+              label="Agreement No."
+              name="agreement_no"
+              rules={[
+                {
+                  // required: props.requiredFields.contract_number,
+                  message: "Please enter the Agreement No.",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} xs={24} style={{ paddingRight: "10px" }}>
+            <Form.Item
+              label="Address"
+              name="address"
+              rules={[
+                {
+                  // required: props.requiredFields.contract_number,
+                  message: "Please enter the address",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col md={12} xs={24} style={{ paddingRight: "10px" }}>
+            <Form.Item
+              label="Order Reference No."
+              name="oder_reference_no"
+              rules={[
+                {
+                  // required: props.requiredFields.contract_number,
+                  message: "Please enter the Order Reference No.",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} xs={24} style={{ paddingRight: "10px" }}>
+            <Form.Item
+              label="Mobile No."
+              name="mobile_no"
+              rules={[
+                {
+                  // required: props.requiredFields.contract_number,
+                  message: "Please enter the Mobile No.",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col md={12} xs={24} style={{ paddingRight: "10px" }}>
+            <Form.Item
+              label="Service Type"
+              name="service_type"
+              rules={[
+                {
+                  // required: props.requiredFields.contract_number,
+                  message: "Please enter the Service Type",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} xs={24} style={{ paddingRight: "10px" }}>
+            <Form.Item
+              label="Invoice period From"
+              name="invoice_period_from"
+              rules={[
+                {
+                  // required: props.requiredFields.contract_number,
+                  message: "Please enter the Invoice period from date",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col md={12} xs={24} style={{ paddingRight: "10px" }}>
+            <Form.Item
+              label="Invoice period To"
+              name="invoice_period_to"
+              rules={[
+                {
+                  // required: props.requiredFields.contract_number,
+                  message: "Please enter the Invoice period to date",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+      <div>
+        <Table
+          // rowSelection={{
+          //   type: selectionType,
+          //   ...rowSelection,
+          // }}
+          columns={columns}
+          dataSource={data}
+          // rowKey={"wo_id"}
+          // loading={loading}
+        />
+      </div>
     </Modal>
   );
 };
